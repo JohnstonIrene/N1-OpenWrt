8dbacfce27120b95e69773842c297a5aeb01eb90
JohnstonIrene <154217523+JohnstonIrene@users.noreply.github.com>
2024-04-16 18:03:12 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -8,11 +8,7 @@ echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/c
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/mosdns
 
 # Add packages
 git clone https://github.com/JohnstonIrene/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
