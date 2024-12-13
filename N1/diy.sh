307534bd2202b55798cfe38967e421e83fff6bd8
JohnstonIrene <154217523+JohnstonIrene@users.noreply.github.com>
2024-12-13 18:37:09 +0800

    N1 OpenWrt

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,9 +6,11 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_gener
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
 
-# Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
-
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
+
+# Update packages
+mv package/amlogic/luci-app-amlogic feeds/luci/applications/
+
+# Clean packages
+rm -rf package/amlogic
